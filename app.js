c450d98f819a27d721dbb020c5571fbc76e0dca1
aditi3089 <aditijsr3089@gmail.com>
2026-04-25 18:20:59 +0530

    footer added

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 //ejs setup
 const path= require("path");
-app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
